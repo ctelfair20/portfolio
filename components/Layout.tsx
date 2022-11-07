0b07f1b180f3debc5ac56d5d95aa7f4557bfcddc
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-11-07 16:25:44 -0600

    creates a layout component to give every page the navbar component

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,15 @@
-const Layout = () => {
-  return (
+import NavBar from "./NavBar";
 
+interface PropsI {
+  // This component only receives one prop -- an array of children
+  children: JSX.Element | JSX.Element[]
+};
+
+const Layout = ({ children }: PropsI) => {
+  return (
+    <>
+      {children}
+    </>
   );
 }
 
